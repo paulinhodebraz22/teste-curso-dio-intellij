264c42209a36b8d6c98b1dceef603cd1abd88ac5
paulinhodebraz22 <paulopsantos.ti@gmail.com>
2022-07-13 09:20:32 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,64 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato() {
+    }
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(idade, gato.idade) && Objects.equals(cor, gato.cor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, idade, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+}
